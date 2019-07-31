2408a0b3c3fa5fefe8b39045a1ff6a9326a4fc8e
serpial <phutchison8@gmail.com>
2019-07-31 17:09:35 +0100

    added movement in the paddles and started movement of the ball

--- Pong.py
+++ Pong.py
@@ -1,11 +1,6 @@
 import turtle
 
-
 class Paddle (turtle.Turtle):
-    up_key = ""
-    down_key = ""
-
-    
     def __init__ (self, starting_x, controls):
         super (Paddle, self).__init__()
 
@@ -13,43 +8,56 @@ class Paddle (turtle.Turtle):
         self.speed(0)
         self.shape("square")
         self.color("black")
-        self.shapesize(1,1)
+        self.shapesize(5,1)
         self.penup()
         self.goto(starting_x, 0)
 
-        up_button = controls[0]
-        down_button = controls[0]
+        self.up_key = controls[0]
+        self.down_key = controls[1]
         
-
     def pressed_up(self):
-        print("Up")
+        self.sety(self.ycor()+20)
         
-
     def pressed_down(self):
-        print("Down")        
+        self.sety(self.ycor()-20)
 
+class Ball (turtle.Turtle):
+    def __init__(self, speed):
+        super (Ball, self).__init__()
+        
+        self.speed(0)
+        self.shape("square")
+        self.color("red")
+        self.penup()
+        self.goto(0, 0)
+        self.dx = speed
+        self.dy = speed
 
+# Window Set-up
 window = turtle.Screen()
 window.title("Pong")
 window.setup(width=800, height=600)
 window.bgcolor("white")
 window.tracer(0)
 
-
+# Paddle Set-up
 distance = 350
 paddles = [
     Paddle(-distance, ["w", "s"]),
     Paddle(distance, ["Up", "Down"])
 ]
-
-
 window.listen()
 for x in range(2):
-    window.onkeypress(paddles[x].pressed_up(), paddles[x].up_key)
-    window.onkeypress(paddles[x].pressed_down(), paddles[x].down_key)
-    pass
+    window.onkeypress(paddles[x].pressed_up, paddles[x].up_key)
+    window.onkeypress(paddles[x].pressed_down, paddles[x].down_key)
+
+# Ball Set-up
+ball = Ball(0.2)
 
 while True:
     window.update()
 
-    
+    # Move ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
